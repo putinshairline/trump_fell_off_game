1c05223ada8d24320fc6e70b6427dea7ba6fb527
putinshairline <85314080+putinshairline@users.noreply.github.com>
2022-11-02 17:35:38 -0700

    Add files via upload

--- spaceinvaders/Background.java
+++ spaceinvaders/Background.java
@@ -11,7 +11,7 @@ import java.awt.Image;
 import javax.imageio.ImageIO;
 
 public class Background {
-	private Image image; //backgroud image
+	  private Image image; //backgroud image
     private int x; //x coordinate
     private int y; //y coordinate
 
@@ -38,22 +38,18 @@ public class Background {
    	//method that draws the image onto the Graphics object passed
     public void draw(Graphics window) {
         //draw the image onto the Graphics reference
-        window.drawImage(image, this.x, this.y, 600, 1080, null);
-        this.y -= 5;
-        
-        if(this.y <= -5) {
-        	this.y = this.y + 1080 * 2;
-        }
-        /*
+        window.drawImage(image, getX(), getY(), 600, 1080, null);
+ 
         //move the x position left for next time
-        this.x -= 18;
+        this.y -= 18;
  
         //check to see if the image has gone off stage left
-        if (this.x <= -1 * 1920) {
- 			//if it has, line it back up so that its left edge is lined up to the right side of the other background image
-            this.x = this.x + 1920 * 2;
+        if (this.y <= -1 * 1080) {
+ 
+            //if it has, line it back up so that its left edge is lined up to the right side of the other background image
+            this.y = this.y + 1080 * 2;
         }//if
- 		*/
+ 
     }//draw
  
     public void setX(int x) {
@@ -66,7 +62,7 @@ public class Background {
         return this.y;
     }//getY
     public int getImageWidth() {
-        return 600;
+        return 1080;
     }//getImageWidth
     
 }//class Background
